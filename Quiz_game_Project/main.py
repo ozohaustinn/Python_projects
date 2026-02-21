1e8b5409a0812a0f6160f19dbdb6d003cdea7d9c
Augustine Ozoemena <ozohaustinn@gmail.com>
2026-02-21 15:13:31 +0100

    Add files via upload

---
+++ Quiz_game_Project/main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for question in question_data:
+    question_bank.append(Question(question["question"], question["correct_answer"]))
+
+quiz = QuizBrain(question_bank)
+
+quiz.next_question()
+
